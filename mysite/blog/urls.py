f4e0f21791c5bd50343fb8473076f7f665ba19b8
Ramesh Parajuli <rameshparajuli619@gmail.com>
2022-09-22 13:36:48 +1000

    Personal Blog

---
+++ mysite/blog/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from .import views 
+urlpatterns = [
+    # APIs to post a comment
+    path('postComment', views.postComment, name='postComment'),
+    path('', views.blogHome, name='blogHome'),
+    path('<str:slug>', views.blogPost, name='blogPost'),
+
+]
